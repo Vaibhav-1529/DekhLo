911dd14fa9d2df7560301f8600d0967a174d653f
Vaibhav-1529 <vaibhavsaini77786@gmail.com>
2025-08-19 13:38:01 +0530

    change for deploy

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,15 +1,17 @@
 //@ts-nocheck
-
 import { NextResponse } from "next/server";
 
-export default function middleware(req){
-    const user=req.cookies.get("user")?.value;
-    const pathname=req.nextUrl.pathname
-    const protectedPaths=["/search","/","/profile"];
-    if(protectedPaths.includes(pathname)){
-        if(!user){
-            return NextResponse.redirect("http://localhost:3000/login");
-        }
+export default function middleware(req) {
+  const user = req.cookies.get("user")?.value;
+  const pathname = req.nextUrl.pathname;
+
+  const protectedPaths = ["/search", "/profile"]; // removed "/"
+
+  if (protectedPaths.includes(pathname)) {
+    if (!user) {
+      return NextResponse.redirect(new URL("/login", req.url));
     }
-    return NextResponse.next();
+  }
+
+  return NextResponse.next();
 }
